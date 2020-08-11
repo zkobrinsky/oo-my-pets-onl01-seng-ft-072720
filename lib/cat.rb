3daad9a0c806bfa4706acf229997601e275c5fde
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-11 17:15:41 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,12 @@
+require "pry"
 class Cat
   attr_reader :name, :owner
 
   def initialize(name, owner)
     @name = name
-    if Owner.all.include?{|o| o.name == owner}
+
+    if Owner.all.include?{|o| o.name == owner
+    binding.pry}
   end
 
 
