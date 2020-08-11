a12f8c1f827e433c0f9b59d45b836d48468e83cc
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-11 17:22:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,33 +1,13 @@
 class Owner
   attr_reader :name, :species
 
-  @@all = []
-
   def initialize(name)
     @name = name
     @species = "human"
-    save
-  end
-
-  def save
-    @@all << self
   end
 
   def say_species
     "I am a #{self.species}."
   end
-
-  def self.all
-    @@all
-  end
-
-  def self.count
-    @@all.count
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-
 
 end
